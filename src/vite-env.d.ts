2177b1e678049505fd6591a02f6496a8ff58c6e6
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-01-30 10:01:22 -0300

    configuração de formatação e variáveis de ambiente

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_VERSION: string
+  readonly VITE_APP_URL: string
+  readonly VITE_API_URL: string
+
+  readonly VITE_GOOGLE_CLIENT_ID: string
+
+  readonly VITE_REVERB_APP_KEY: string
+  readonly VITE_REVERB_HOST: string
+  readonly VITE_REVERB_PORT: string
+  readonly VITE_REVERB_SCHEME: 'http' | 'https'
+
+  readonly VITE_TENANT_SLUG: string
+  readonly VITE_APP_MODE: 'client' | 'admin'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
